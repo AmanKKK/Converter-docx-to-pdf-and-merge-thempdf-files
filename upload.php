33e5fdb21553ef43b83319ab409cba6f453651a6
Aman_Shygaibaev <52895897+AmanKKK@users.noreply.github.com>
2020-05-25 19:23:25 +0300

    final version
    Done

--- upload.php
+++ upload.php
@@ -24,6 +24,7 @@ if(isset($_FILES['uploadedFile']) && isset($_FILES['uploadedFile1'])){
    echo 'error';
 }
 
+
 /* Данные полученных файлов */
 $fileTmpPath = $_FILES['uploadedFile']['tmp_name'];
 $fileName = $_FILES['uploadedFile']['name'];
@@ -44,44 +45,39 @@ $fileExtension1 = strtolower(end($fileNameCmps1));
 $uploadFileDir1 = 'uploaded_files/';
 $dest_path1 = $uploadFileDir1 . $fileName1;
 
+/*пути для получения и сохранения файлов */
+$absolutePath='C:/OSPanel/domains/part2/'; //для команды soffice, получение файла для конвертации;
+$uploading_path='C:/OSPanel/domains/part2/uploaded_files';// путь, где будет сконвертированный pdf файл;
+$path_for_merging_pdf='C:OSPanel/domains/part2/uploaded_files/';
+
+
+
 
 
 $allowedfileExtensions = array('pdf','doc','docx');
 
-echo '<br>'.'dest_path: '. $dest_path;
-echo '<br>'.'dest_path1: '. $dest_path1;
 
-$absolutePath='C:/OSPanel/domains/part2/'; //для команды soffice, получение файла для конвертации;
-$uploading_path='C:/OSPanel/domains/part2/uploaded_files';// путь, где будет сконвертированный pdf файл;
-$path_for_merging_pdf='C:OSPanel/domains/part2/uploaded_files/';
-$path_for_pdf=$path_for_merging_pdf.$fileName;//путь к pdf файлу;
-//$merged_file_name=$fileNameCmps[0].'.pdf'; //название "склеинового" файла pdf;
+
+
 
 if (in_array($fileExtension, $allowedfileExtensions)) { // проверка на соотвествие расширений;
    move_uploaded_file($fileTmpPath, $dest_path);
  
-   if($fileNameCmps[1]==='docx'){
-      $PathToFile=$absolutePath.$dest_path;
-      
-      chdir('C:/OSPanel/LibreOffice/program');
-      `soffice --headless --convert-to pdf $PathToFile --outdir $uploading_path `;
-      $new_file_name=$fileNameCmps[0].'.pdf'; //название "склеиного" файла pdf;
-
-   }else{echo '<br>' .'error0';}
-
+   
 }else{
   echo 'Invalid fileExtension'.'<br>';
   echo 'You can upload only files with : jpg, gif, png, zip, txt, xls, doc, docx extensions';
 }
 
 if (in_array($fileExtension1, $allowedfileExtensions)) { 
-  chdir('C:/OSPanel/domains/part2');
+  
   move_uploaded_file($fileTmpPath1, $dest_path1);
   
   if($fileNameCmps1[1]==='docx'){
    $PathToFile1=$absolutePath.$dest_path1;
    chdir('C:/OSPanel/LibreOffice/program');
    `soffice --headless --convert-to pdf $PathToFile1 --outdir $uploading_path`;
+   $new_file_name=$fileNameCmps1[0].'.pdf';
 
   
    
@@ -95,8 +91,8 @@ chdir('C:/OSPanel/domains/part2');
 
 /*----------------------------------------------------------------------------------------------------- */
 
-$path_to_first_pdf = $path_for_merging_pdf.$new_file_name;
-$path_to_second_pdf=$path_for_merging_pdf.$fileName1;
+$path_to_first_pdf = $path_for_merging_pdf.$fileName;
+$path_to_second_pdf=$path_for_merging_pdf.$new_file_name;
 
 $files=array(
    $path_to_first_pdf,
@@ -122,66 +118,31 @@ foreach ($files AS $file) {
        // use the imported page
        $pdf->useTemplate($templateId);
 
-       $pdf->SetFont('Helvetica');
-       $pdf->SetXY(5, 5);
-       $pdf->Write(8, 'A simple concatenation demo with FPDI');
+       
    }
 }
 
 // Output the new PDF
 
 
+$PATH_for_merged_file='C:/OSPanel/domains/part2/uploaded_files/merged_file/';
+$NAME_for_merged_file=$PATH_for_merged_file.$fileName;
+$pdf->Output("F",$NAME_for_merged_file);
 
+$for_deleting=$path_for_merging_pdf.$fileName;
+$for_deleting1=$path_for_merging_pdf.$fileName1;
+$for_deleting2=$path_for_merging_pdf.$new_file_name;
 
-$pdf->Output("F",'C:/OSPanel/domains/part2/uploaded_files/merged_file/new.pdf');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* не разобрался
-$path_to_pdf_file=$path_for_merging_pdf.$newFileName; //путь к сконвертированному файлу pdf ;
-
-$fileArray=array(
-   $path_to_pdf_file,
-   $path_for_pdf
+$delete=array(
+   $for_deleting,
+   $for_deleting1,
+   $for_deleting2,
 );
 
-$savedir='C:/OSPanel/domains/part2/uploaded_files/merged_file/';
-$outputName=$savedir.$merged_file_name;
-
-chdir('C:/OSPanel/gs/gs9.25/bin');
-$cmd="gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=$outputName";
-foreach($fileArray as $file){
-   $cmd.=$file. "musk.pdf";
+foreach($delete as $action){
+   unlink($action);
 }
-$result=shell_exec($cmd);
-*/
+
 
 
 
